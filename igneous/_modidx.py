dd7f69930ec7825e968d6675ad66baa4f915be30
kelvinltse <kelvinltse@gmail.com>
2024-12-02 14:11:45 -0800

    relative path

--- igneous/_modidx.py
+++ igneous/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://kelvinltse.github.io',
                 'git_url': 'https://github.com/kelvinltse/igneous',
                 'lib_path': 'igneous'},
-  'syms': {'igneous.core': {'igneous.core.foo': ('core.html#foo', 'igneous/core.py')}}}
+  'syms': { 'igneous.core': {'igneous.core.foo': ('igneous.html#foo', 'igneous/core.py')},
+            'igneous.igneous': { 'igneous.igneous.classify_image': ('igneous.html#classify_image', 'igneous/igneous.py'),
+                                 'igneous.igneous.get_model_path': ('igneous.html#get_model_path', 'igneous/igneous.py')}}}
